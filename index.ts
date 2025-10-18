2d72a4f21bbafe71c2b45336ea4bd8c2cabc8067
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-10-19 00:22:42 +0300

    bun lock bun version print

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ Bun.serve({
     port: PORT,
     fetch(req) {
         const url = new URL(req.url);
-        if (url.pathname === "/") return new Response("Home page!");
+        if (url.pathname === "/") return new Response(`Home page! Bun version: ${Bun.version}`);
         if (url.pathname === "/blog") return new Response("Blog!");
         if (url.pathname === "/about") return new Response("About!");
         return new Response("404!");
